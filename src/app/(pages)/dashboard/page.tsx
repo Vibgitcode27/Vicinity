893123f00c1fde8567ea8260826239b38c01739e
Vibhor <vibgitcode27@gmail.com>
2024-04-02 04:17:34 +0530

    added right sider

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,8 +1,11 @@
 "use client"
+import { Avatar } from 'antd';
 import "../../styles/dashboard.css"
 import React, { useState } from 'react';
 import { MenuFoldOutlined , MenuUnfoldOutlined } from '@ant-design/icons';
 import { Layout, Button, Flex } from 'antd';
+import { AliwangwangFilled , BellOutlined , MessageOutlined } from '@ant-design/icons';
+
 import { Sidebar } from "../../(components)/Sidebar"
 import { MainHeader } from "../../(components)/Header"
 import { MainContent } from "../../(components)/MainContent"
@@ -15,31 +18,54 @@ export default function Dashboard() {
   const [collapsed , setCollapsed] = useState<boolean>(true)
   return (
     <Layout>
-        <Sider
-          style={{ backgroundColor : "#fff"}}
-          trigger={null} 
-          collapsible 
-          collapsed={collapsed} 
-          className='sider'>
-            <Sidebar/>
-            <Button 
-              type="text"
-              icon={collapsed ? < MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
-              onClick={() => { setCollapsed(!collapsed)}}
-              className="trigger-btn"
-            />
-        </Sider>
-      <Layout>
+      <Sider
+        style={{ backgroundColor: "#fff" }}
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        className='sider'
+      >
+        <Sidebar />
+        <Button
+          type="text"
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={() => { setCollapsed(!collapsed) }}
+          className="trigger-btn"
+        />
+      </Sider>
+
+      <Layout style={{ backgroundColor: "#f1f2ff70" }}>
         <Header className='header'>
-          <MainHeader/>
+          <MainHeader />
         </Header>
         <Content className='content'>
           <Flex gap="large">
-            <MainContent/>
-            <SideContent/>
+            <MainContent />
+            <SideContent />
           </Flex>
         </Content>
       </Layout>
+    
+      <Sider
+        style={{
+          backgroundColor: "#fff",
+          display: "flex",
+          alignContent: "center",
+          justifyContent: "center",
+          paddingTop: "40px",
+          width: "auto" // Set width to "auto" for automatic adjustment
+        }}
+      >
+        <Flex align="center" justify="space-between" gap="20px">
+          <BellOutlined className='profile-icons' />
+          <Avatar
+            size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+            icon={<AliwangwangFilled />}
+          />
+          <MessageOutlined className='profile-icons' />
+        </Flex>
+      </Sider>
     </Layout>
+
   );
 }
