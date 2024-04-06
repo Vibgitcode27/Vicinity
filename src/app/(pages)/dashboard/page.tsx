634db54b38fbfb93d8d9a12b21b57b901d01c371
Shreyyaa17 <shreyaag402@gmail.com>
2024-04-06 10:55:54 +0530

    added pages to dashboard

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -3,8 +3,8 @@ import "../../styles/dashboard.css"
 import { Avatar, Typography } from 'antd';
 import "../../styles/dashboard.css"
 import React, { useState } from 'react';
-import { MenuFoldOutlined , MenuUnfoldOutlined } from '@ant-design/icons';
-import { Layout, Button, Flex , Card , Progress } from 'antd';
+import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
+import { Layout, Button, Flex, Card, Progress } from 'antd';
 import { AliwangwangFilled } from '@ant-design/icons';
 
 import { MainHeader } from "../../(components)/Header"
@@ -12,35 +12,42 @@ import { MainContent } from "../../(components)/MainContent"
 import { SideProfile } from '../../(components)/SideProfile';
 
 import { Menu } from "antd";
-import {WindowsFilled , RocketFilled , CompassFilled , FireFilled,} from "@ant-design/icons";
+import { WindowsFilled, RocketFilled, CompassFilled, FireFilled, } from "@ant-design/icons";
 
 const items = [
   {
-      key : 1 ,
-      icon : <WindowsFilled className="sidebar-icons"/> ,
-      label : "Dashboard"
+    key: 1,
+    icon: <WindowsFilled className="sidebar-icons" />,
+    label: "Dashboard"
   },
   {
-      key : 2 ,
-      icon : <CompassFilled className="sidebar-icons"/> ,
-      label : "Explore"
-  } ,
+    key: 2,
+    icon: <CompassFilled className="sidebar-icons" />,
+    label: "Explore"
+  },
   {
-      key : 3 ,
-      icon : <FireFilled className="sidebar-icons"/> ,
-      label : "For You"
-  } ,
+    key: 3,
+    icon: <FireFilled className="sidebar-icons" />,
+    label: "For You"
+  },
   {
-      key : 4 ,
-      icon : <RocketFilled  className="sidebar-icons"/> ,
-      label : "Logout" 
-  } ,
+    key: 4,
+    icon: <RocketFilled className="sidebar-icons" />,
+    label: "Logout"
+  },
 ]
 
 const { Header, Content, Sider } = Layout;
 export default function Dashboard() {
 
-  const [collapsed , setCollapsed] = useState<boolean>(true)
+  const [collapsed, setCollapsed] = useState<boolean>(true)
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+
+  const handleMenuClick = (index: number) => {
+    setSelectedIndex(index);
+    console.log(selectedIndex);
+  }
+
   return (
     <Layout>
       <Sider
@@ -52,19 +59,24 @@ export default function Dashboard() {
       >
         {/* Inside Sider */}
         <div>
-            <Flex align="center" justify="center">
-                <div className="logo">
-                    <AliwangwangFilled style={{color : "#051bc7ab" , position:"relative" ,  top : "-9px"}}/>
-                </div>
-            </Flex>
-            <Menu
-                style={{ backgroundColor : "#fff"}}
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                className="menu-bar"
-                items= {items}
-            >
-            </Menu>
+          <Flex align="center" justify="center">
+            <div className="logo">
+              <AliwangwangFilled style={{ color: "#051bc7ab", position: "relative", top: "-9px" }} />
+            </div>
+          </Flex>
+          <Menu
+            style={{ backgroundColor: "#fff" }}
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            className="menu-bar"
+            selectedKeys={[String(selectedIndex + 1)]}
+          >
+            {items.map(item => (
+              <Menu.Item key={item.key} icon={item.icon} onClick={() => handleMenuClick(item.key - 1)}>
+                {item.label}
+              </Menu.Item>
+            ))}
+          </Menu>
         </div>
         <Button
           type="text"
@@ -73,20 +85,33 @@ export default function Dashboard() {
           className="trigger-btn"
         />
       </Sider>
-
       <Layout style={{ backgroundColor: "#f1f2ff70" }}>
         <Header className='header'>
           <MainHeader />
         </Header>
         <Content className='content'>
           <Flex gap="large">
-            <MainContent />
-            {/* <SideContent /> */}
+
+            {/* DASHBOARD CONTENT */}
+            {selectedIndex == 0 && (
+              <MainContent />
+            )}
+            {/* EXPLORE CONTENT */}
+            {selectedIndex == 1 && (
+              <span>This is Explore</span>
+            )}
+            {/* FOR YOU CONTENT */}
+            {selectedIndex == 2 && (
+              <span>This is For You</span>
+            )}
+            {/* LOGOUT METHOD */}
+            {selectedIndex == 3 && (
+              <span>This is Logout</span>
+            )}
           </Flex>
         </Content>
       </Layout>
-      <SideProfile/>
+      <SideProfile />
     </Layout>
-
   );
 }
