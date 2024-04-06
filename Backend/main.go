58e1be7588d7655777245fd4c19f7cf8498ebdca
Shashwat Singh <shashwat123student@gmail.com>
2024-04-06 09:53:37 +0530

    Created postUser Route

--- Backend/main.go
+++ Backend/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/WH055_MindNotWorking/db"
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"net/http"
+)
+
 type User struct {
 	Fname      string `json:"fname"`
 	Lname      string `json:"lname"`
@@ -41,4 +51,60 @@ type Book struct {
 	Username string `json:"username"`
 	PostID   string `json:"postID"`
 	Title    string `json:"title"`
+}
+
+func PostUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		http.Error(w, "Failed to connect to database", http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		if err := client.Prisma.Disconnect(); err != nil {
+			panic(err)
+		}
+	}()
+
+	ctx := context.Background()
+
+	createdUser, err := client.User.CreateOne(
+		db.User.LastName.Set(user.Lname),
+		db.User.FirstName.Set(user.Fname),
+		db.User.ProfilePicture.Set(user.ProfilePic),
+		db.User.Location.Set(user.Location),
+		db.User.Bio.Set(user.Bio),
+		db.User.Email.Set(user.Email),
+		db.User.Username.Set(user.Username),
+	).Exec(ctx)
+
+	if err != nil {
+		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		return
+	}
+
+	result, _ := json.MarshalIndent(createdUser, "", "  ")
+	fmt.Printf("Created user: %s\n", result)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("User created successfully"))
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/postuser", PostUser).Methods("POST")
+
+	corsHandler := cors.Default().Handler(r)
+
+	http.ListenAndServe(":4000", corsHandler)
 }
