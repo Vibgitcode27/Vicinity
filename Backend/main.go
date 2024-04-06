7ebb880687785eea5d05cca6a9fe40a78d67ab90
Shashwat Singh <shashwat123student@gmail.com>
2024-04-06 09:49:56 +0530

    Created important structures

--- Backend/main.go
+++ Backend/main.go
@@ -1,1 +1,44 @@
-package Backend
+package main
+
+type User struct {
+	Fname      string `json:"fname"`
+	Lname      string `json:"lname"`
+	Location   string `json:"location"`
+	Bio        string `json:"bio"`
+	Email      string `json:"email"`
+	Username   string `json:"username"`
+	ProfilePic string `json:"profilePic"`
+}
+
+type BookResponse struct {
+	BookID string `json:"bookID"`
+	Title  string `json:"title"`
+}
+
+type PostId struct {
+	PostId string `json:"postId"`
+}
+
+type Post struct {
+	Picture                    string `json:"picture"`
+	Description                string `json:"description"`
+	Duration                   string `json:"duration"`
+	RequirementsAndRestriction string `json:"requirementsAndRestriction"`
+	Location                   string `json:"location"`
+	Cost                       string `json:"cost"`
+	Username                   string `json:"username"`
+}
+
+type Dude struct {
+	Username string `json:"username"`
+}
+
+type City struct {
+	Location string `json:"location"`
+}
+
+type Book struct {
+	Username string `json:"username"`
+	PostID   string `json:"postID"`
+	Title    string `json:"title"`
+}
